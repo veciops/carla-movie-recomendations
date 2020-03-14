298c9285f5b1be5fb548a13b1e56f84280e19169
asus-vecio <veciops@gmail.com>
2020-03-14 02:05:15 -0300

    funcionando 8

--- recomendacao.py
+++ recomendacao.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
-from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
+from flask_restful import Resource, Api
 
 import pandas as pd
 import numpy as np
